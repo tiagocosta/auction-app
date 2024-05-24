ae380956e768173769708cb5bd85cd7710c7ecc3
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-05-24 16:49:07 -0300

    creates auction repository interface

---
+++ internal/entity/auction_entity/auction_repository_interface.go
@@ -0,0 +1,13 @@
+package auction_entity
+
+import (
+	"context"
+
+	"github.com/tiagocosta/auction-app/internal/internal_error"
+)
+
+type AuctionRepositoryInterface interface {
+	CreateAuction(ctx context.Context, auction Auction) *internal_error.InternalError
+	FindAuctionById(ctx context.Context, id string) (*Auction, *internal_error.InternalError)
+	FindAuctions(ctx context.Context, status AuctionStatus, category string, productName string) ([]Auction, *internal_error.InternalError)
+}
