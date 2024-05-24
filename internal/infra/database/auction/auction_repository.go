2759ba6662b9c92c4aff0e2589c37fe849943e05
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-05-24 11:58:27 -0300

    creates auction repository

---
+++ internal/infra/database/auction/auction_repository.go
@@ -0,0 +1,26 @@
+package auction
+
+import (
+	"github.com/tiagocosta/auction-app/internal/entity/auction_entity"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type AuctionEntityMongo struct {
+	Id          string                          `bson:"_id"`
+	ProductName string                          `bson:"product_name"`
+	Category    string                          `bson:"category"`
+	Description string                          `bson:"description"`
+	Condition   auction_entity.ProductCondition `bson:"condition"`
+	Status      auction_entity.AuctionStatus    `bson:"status"`
+	Timestamp   int64                           `bson:"timestamp"`
+}
+
+type AuctionRepository struct {
+	Collection *mongo.Collection
+}
+
+func NewAuctionRepository(database *mongo.Database) *AuctionRepository {
+	return &AuctionRepository{
+		Collection: database.Collection("auctions"),
+	}
+}
