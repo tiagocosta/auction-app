c5ee42913beb23cd1d8a33e2716525dd10421724
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-05-24 23:14:59 -0300

    adding new and validation functions

--- internal/entity/auction_entity/auction_entity.go
+++ internal/entity/auction_entity/auction_entity.go
@@ -1,6 +1,11 @@
 package auction_entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tiagocosta/auction-app/internal/internal_error"
+)
 
 type ProductCondition int
 
@@ -25,6 +30,34 @@ type Auction struct {
 	Condition   ProductCondition
 	Status      AuctionStatus
 	Timestamp   time.Time
+}
+
+func NewAuction(productName, category, description string, condition ProductCondition) (*Auction, *internal_error.InternalError) {
+	auction := &Auction{
+		Id:          uuid.NewString(),
+		ProductName: productName,
+		Category:    category,
+		Description: description,
+		Condition:   condition,
+		Status:      Active,
+		Timestamp:   time.Now(),
+	}
+
+	if err := auction.Validate(); err != nil {
+		return nil, err
+	}
+
+	return auction, nil
+}
+
+func (auction *Auction) Validate() *internal_error.InternalError {
+	if len(auction.ProductName) <= 1 ||
+		len(auction.Category) <= 2 ||
+		len(auction.Description) <= 10 &&
+			(auction.Condition != New && auction.Condition != Used && auction.Condition != Refurbished) {
+		return internal_error.NewBadRequestError("invalid auction object")
+	}
+	return nil
 }
 
 func (auction *Auction) IsActive() bool {
