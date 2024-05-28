f6a8da0c76c3783d4ac94331f95b0c21a6805a16
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-05-27 21:40:35 -0300

    implements constructor and validator

--- internal/entity/bid_entity/bid_entity.go
+++ internal/entity/bid_entity/bid_entity.go
@@ -1,6 +1,11 @@
 package bid_entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/tiagocosta/auction-app/internal/internal_error"
+)
 
 type Bid struct {
 	Id        string
@@ -8,4 +13,32 @@ type Bid struct {
 	AuctionId string
 	Amount    float64
 	Timestamp time.Time
+}
+
+func NewBid(userId, auctionId string, amount float64) (*Bid, *internal_error.InternalError) {
+	bid := &Bid{
+		Id:        uuid.NewString(),
+		UserId:    userId,
+		AuctionId: auctionId,
+		Amount:    amount,
+		Timestamp: time.Now(),
+	}
+	err := bid.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return bid, nil
+}
+
+func (bid *Bid) Validate() *internal_error.InternalError {
+	if err := uuid.Validate(bid.Id); err != nil {
+		return internal_error.NewBadRequestError("invalid user id")
+	}
+	if err := uuid.Validate(bid.AuctionId); err != nil {
+		return internal_error.NewBadRequestError("invalid auction id")
+	}
+	if bid.Amount <= 0 {
+		return internal_error.NewBadRequestError("invalid amount value")
+	}
+	return nil
 }
