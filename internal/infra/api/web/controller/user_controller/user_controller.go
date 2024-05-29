a70d31d2dbc7a8d7e0c24f41579331a6e5694c4b
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-05-29 13:44:28 -0300

    usecase interface as parameter for user controller

--- internal/infra/api/web/controller/user_controller/user_controller.go
+++ internal/infra/api/web/controller/user_controller/user_controller.go
@@ -3,10 +3,10 @@ package user_controller
 import "github.com/tiagocosta/auction-app/internal/usecase/user_usecase"
 
 type UserController struct {
-	UserUseCase user_usecase.UserUseCase
+	UserUseCase user_usecase.UserUseCaseInterface
 }
 
-func NewUserController(userUseCase user_usecase.UserUseCase) *UserController {
+func NewUserController(userUseCase user_usecase.UserUseCaseInterface) *UserController {
 	return &UserController{
 		UserUseCase: userUseCase,
 	}
