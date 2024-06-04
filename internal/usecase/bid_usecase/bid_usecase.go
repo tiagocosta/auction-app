1ee08218be3454f951919c829303eee1bd6068a2
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-06-04 15:42:37 -0300

    refactoring bid usecase

--- internal/usecase/bid_usecase/bid_usecase.go
+++ internal/usecase/bid_usecase/bid_usecase.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/tiagocosta/auction-app/configuration/logger"
 	"github.com/tiagocosta/auction-app/internal/entity/bid_entity"
 	"github.com/tiagocosta/auction-app/internal/internal_error"
 )
@@ -65,6 +66,44 @@ func getMaxBatchSize() int {
 		return 5
 	}
 	return maxBatchSize
+}
+
+var bidBatch []bid_entity.Bid
+
+func (uc *BidUseCase) startBidBatchRoutine(ctx context.Context) {
+	go func() {
+		defer close(uc.BidChannel)
+		for {
+			select {
+			case bid, ok := <-uc.BidChannel:
+				if !ok {
+					if len(bidBatch) > 0 {
+						err := uc.BidRepository.CreateBid(ctx, bidBatch)
+						if err != nil {
+							logger.Error("error trying to process bid batch list", err)
+						}
+					}
+					return
+				}
+				bidBatch = append(bidBatch, bid)
+				if len(bidBatch) > uc.MaxBatchSize {
+					err := uc.BidRepository.CreateBid(ctx, bidBatch)
+					if err != nil {
+						logger.Error("error trying to process bid batch list", err)
+					}
+					bidBatch = nil
+					uc.Timer.Reset(uc.BatchInsertInterval)
+				}
+			case <-uc.Timer.C:
+				err := uc.BidRepository.CreateBid(ctx, bidBatch)
+				if err != nil {
+					logger.Error("error trying to process bid batch list", err)
+				}
+				bidBatch = nil
+				uc.Timer.Reset(uc.BatchInsertInterval)
+			}
+		}
+	}()
 }
 
 type BidUseCaseInterface interface {
